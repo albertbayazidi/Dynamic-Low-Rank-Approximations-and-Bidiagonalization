7d77f538cc3d5c8bf2675d20498fd2a8a61cb125
Peder Brekke <pederbrekke01@gmail.com>
2023-10-31 09:28:37 +0100

    La til code som generer A-matrisa for task 4

---
+++ Example_matrices.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.linalg import expm
+from scipy.sparse import diags
+
+def generateA(epsilon):
+    """
+    Generates A1 and A2 for the bigger A-matrix
+    input: 
+    epsilon - weighting variable 
+    output:
+    The matrix A1 or A2
+    """
+    first_block =  np.random.rand(10,10)/2 + np.identity(10)
+    A = epsilon * np.random.rand(100,100)
+    A [:10,:10] += first_block
+    return A
+
+
+
+def A(t, epsilon = 1/2): 
+    """
+    Generate A-matrix as described in task 4
+    input: 
+    t - time variable
+    epsilon - weighting variable for A1 and A2
+    output:
+    the matrix A evaluated at time t
+    """
+    A1 = generateA(epsilon)
+    A2 = generateA(epsilon)
+    
+    Id = np.identity(100)
+    
+    T1 = np.diag(-np.ones(99), k=1) + np.diag(np.ones(99), k=-1)
+    T2 = np.diag(-np.ones(99)/2, k=1) + np.diag(-np.ones(98), k=2) + np.diag(np.ones(99)/2, k=-1) + np.diag(np.ones(98), k=-2)
+    
+    Q1 = lambda t :  Id * expm(t *T1)
+    Q2 = lambda t :  Id * expm(t *T2)
+    
+    return Q1(t) @ (A1 + np.exp(t) * A2) @ Q2(t).T
