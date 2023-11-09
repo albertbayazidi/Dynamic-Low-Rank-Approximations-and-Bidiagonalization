0a0ae911353e2a9fbe3170b7a27974e3e50c8a82
Peder Brekke <pederbrekke01@gmail.com>
2023-11-09 10:14:59 +0100

    la til matriser for oppg 5

--- Example_matrices.py
+++ Example_matrices.py
@@ -16,6 +16,7 @@ def generateA(epsilon):
     A [:10,:10] = first_block
     return A
 
+# Generate A1 and A2 once, to be used in all experiments
 np.random.seed(1)
 A1 = generateA(1/2)
 A2 = generateA(1/2)
@@ -59,3 +60,46 @@ def A_dot(t):
     
     # using chain rule
     return (T1 @ Q1(t) @ A1 + np.exp(t)*(Id + T1)@ Q1(t) @ A2) @ Q2(t).T + (Q1(t) @ A1 + np.exp(t) * Q1(t) @ A2) @ (T2@Q2(t)).T
+
+
+def A_2(t):
+    """
+    Generate A-matrix as described in task 5
+    input: 
+    t - time variable
+    epsilon - weighting variable for A1 and A2
+    output:
+    the matrix A evaluated at time t
+    """
+    
+    Id = np.identity(100)
+    
+    T1 = np.diag(-np.ones(99), k=1) + np.diag(np.ones(99), k=-1)
+    T2 = np.diag(-np.ones(99)/2, k=1) + np.diag(-np.ones(98), k=2) + np.diag(np.ones(99)/2, k=-1) + np.diag(np.ones(98), k=-2)
+    
+    Q1 = lambda t :  Id * expm(t *T1)
+    Q2 = lambda t :  Id * expm(t *T2)
+    
+    return Q1(t) @ (A1 + np.cos(t) * A2) @ Q2(t).T
+
+
+def A_2_dot(t):
+    """
+    Generate the time-derivative of A-matrix as described in task 4
+    input: 
+    t - time variable
+    epsilon - weighting variable for A1 and A2
+    output:
+    the derivative of matrix A evaluated at time t
+    """
+
+    Id = np.identity(100)
+    
+    T1 = np.diag(-np.ones(99), k=1) + np.diag(np.ones(99), k=-1)
+    T2 = np.diag(-np.ones(99)/2, k=1) + np.diag(-np.ones(98), k=2) + np.diag(np.ones(99)/2, k=-1) + np.diag(np.ones(98), k=-2)
+    
+    Q1 = lambda t :  Id * expm(t *T1)
+    Q2 = lambda t :  Id * expm(t *T2)
+    
+    # using chain rule
+    return (T1 @ Q1(t) @ A1 + -np.sin(t) * Q1(t) @ A2 + np.cos(t) * T1 @ Q1(t) @ A2) @ Q2(t).T + (Q1(t) @ A1 + np.cos(t) * Q1(t) @ A2) @ (T2@Q2(t)).T
